5af278261b1a142b7bc36a916cc4e60af44b78f7
Shiva Gupta <142409104+Shiva2997@users.noreply.github.com>
2026-06-25 12:12:32 +0530

    Add files via upload

---
+++ Day 1/simple.py
@@ -0,0 +1,14 @@
+print("calcuylatoe")
+a = int(input("First input: "))
+b = int(input("Second input: "))
+
+
+print("Addition: " , a+b )
+print("Subtraction: " , a-b )
+print("Multiplication: " , a*b )
+print("Division: " , a/b )
+print("Modulus: " , a%b )
+print("Exponentiation: " , a**b )
+
+
+
