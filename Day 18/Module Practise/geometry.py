623252f48dc045cf78e7a47ed69040480e147f01
Shiva Gupta <142409104+Shiva2997@users.noreply.github.com>
2026-07-18 10:15:10 +0530

    Add files via upload

---
+++ Day 18/Module Practise/geometry.py
@@ -0,0 +1,8 @@
+def area_square(a):
+    return a ** 2
+
+def area_rectangle(length, width):
+    return length * width
+
+def area_triangle(base, height):
+    return 0.5 * base * height
---
+++ Day 18/Module Practise/main.py
@@ -0,0 +1,42 @@
+import calculator
+
+result = calculator.add(5, 3)
+print(result)
+
+result = calculator.subtract(5, 3)
+print(result)
+
+result = calculator.multiply(5, 3)
+
+
+import statistics
+marks = [ 87 , 89 , 87 , 32 , 43 ,98]
+print(statistics.mean(marks))
+print(statistics.median((marks)))
+
+import keyword
+
+#print(keyword.kwlist)
+#print(keyword.iskeyword("for"))
+
+import os
+print(os.getcwd())
+
+import math
+print(math_file_)
+
+
+pip install numpy
+pip install pandas
+pip intall matplotlib
+
+# Built a simple Billing System . The objective is not to built a full application , but to understand how different Python modules colloborate in a real project.
+
+"""
+ewdnjkw 
+wekjcnkwjn
+print("Hello World")
+
+"""
+
+
